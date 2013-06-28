3fb505da3bef5d687fbfdd5943781f31611b56c0
Joe Stringer <joe@wand.net.nz>
2013-06-28 14:30:26 +0900

    Conform to pep8

--- ofinterface.py
+++ ofinterface.py
@@ -2,11 +2,12 @@ import logging
 
 from rflib.defs import *
 from binascii import *
-from rflib.types.Match import * 
+from rflib.types.Match import *
 from rflib.types.Action import *
 from rflib.types.Option import *
 
 log = logging.getLogger('ryu.app.rfproxy')
+
 
 def create_default_flow_mod(dp, cookie=0, cookie_mask=0, table_id=0,
                             command=None, idle_timeout=0, hard_timeout=0,
@@ -15,125 +16,134 @@ def create_default_flow_mod(dp, cookie=0, cookie_mask=0, table_id=0,
                             inst_type=None, out_port=None, out_group=None,
                             flags=0, inst=[]):
 
-  if command is None:
-    command = dp.ofproto.OFPFC_ADD
+    if command is None:
+        command = dp.ofproto.OFPFC_ADD
 
-  if inst is []:
-    if inst_type is None:
-      inst_type = dp.ofproto.OFPIT_APPLY_ACTIONS
+    if inst is []:
+        if inst_type is None:
+            inst_type = dp.ofproto.OFPIT_APPLY_ACTIONS
 
-    if actions is not None:
-      inst = [dp.ofproto_parser.OFPInstructionActions(inst_type, actions)]
+        if actions is not None:
+            inst = [dp.ofproto_parser.OFPInstructionActions(inst_type,
+                                                            actions)]
 
-  if match is None:
-    match = dp.ofproto_parser.OFPMatch()
+    if match is None:
+        match = dp.ofproto_parser.OFPMatch()
 
-  if out_port is None:
-    out_port = dp.ofproto.OFPP_ANY
+    if out_port is None:
+        out_port = dp.ofproto.OFPP_ANY
 
-  if out_group is None:
-    out_group = dp.ofproto.OFPG_ANY
+    if out_group is None:
+        out_group = dp.ofproto.OFPG_ANY
 
-  return dp.ofproto_parser.OFPFlowMod(dp, cookie, cookie_mask,
-                                      table_id, command,
-                                      idle_timeout, hard_timeout,
-                                      priority, buffer_id,
-                                      out_port, out_group,
-                                      flags, match, inst)
+    return dp.ofproto_parser.OFPFlowMod(dp, cookie, cookie_mask,
+                                        table_id, command,
+                                        idle_timeout, hard_timeout,
+                                        priority, buffer_id,
+                                        out_port, out_group,
+                                        flags, match, inst)
+
 
 def create_flow_mod(dp, mod, matches, actions, options):
-  flow_mod = create_default_flow_mod(dp)
-  add_command(flow_mod, mod)
-  add_matches(flow_mod, matches)
-  add_actions(flow_mod, actions)
-  add_options(flow_mod, options)
-  return flow_mod
+    flow_mod = create_default_flow_mod(dp)
+    add_command(flow_mod, mod)
+    add_matches(flow_mod, matches)
+    add_actions(flow_mod, actions)
+    add_options(flow_mod, options)
+    return flow_mod
+
 
 def add_command(flow_mod, mod):
-  if mod == RMT_ADD:
-    pass
-  elif mod == RMT_DELETE:
-    flow_mod.command = flow_mod.datapath.ofproto.OFPFC_DELETE_STRICT
+    if mod == RMT_ADD:
+        pass
+    elif mod == RMT_DELETE:
+        flow_mod.command = flow_mod.datapath.ofproto.OFPFC_DELETE_STRICT
+
 
 def add_matches(flow_mod, matches):
-  for m in matches:
-    match = Match.from_dict(m)
-    if match._type == RFMT_IPV4:
-      value = bin_to_int(match._value)
-      addr = value >> 32
-      mask = value & ((1 << 32) - 1)
-      flow_mod.match.set_dl_type(ETHERTYPE_IP)
-      flow_mod.match.set_ipv4_dst_masked(addr, mask)
-    elif match._type == RFMT_IPV6:
-      v = match._value
-      addr = tuple((ord(v[i]) << 8) | ord(v[i + 1]) for i in range(0, 16, 2))
-      mask = tuple((ord(v[i]) << 8) | ord(v[i + 1]) for i in range(16, 32, 2))
-      flow_mod.match.set_dl_type(ETHERTYPE_IPV6)
-      flow_mod.match.set_ipv6_dst_masked(addr, mask)
-    elif match._type == RFMT_ETHERNET:
-      flow_mod.match.set_dl_dst(match._value)
-    elif match._type == RFMT_ETHERTYPE:
-      flow_mod.match.set_dl_type(bin_to_int(match._value))
-    elif match._type == RFMT_NW_PROTO:
-      flow_mod.match.set_ip_proto(bin_to_int(match._value))
-    elif match._type == RFMT_TP_SRC:
-      flow_mod.match.set_ip_proto(IPPROTO_TCP)
-      flow_mod.match.set_tcp_src(bin_to_int(match._value))
-    elif match._type == RFMT_TP_DST:
-      flow_mod.match.set_ip_proto(IPPROTO_TCP)
-      flow_mod.match.set_tcp_dst(bin_to_int(match._value))
-    elif match._type == RFMT_IN_PORT:
-      flow_mod.match.set_in_port(bin_to_int(match._value))
-    elif TLV.optional(match):
-        log.info("Dropping unsupported Match (type: %s)" % match._type)
-    else:
-        log.warning("Failed to serialise Match (type: %s)" % match._type)
-        return
+    for m in matches:
+        match = Match.from_dict(m)
+        if match._type == RFMT_IPV4:
+            value = bin_to_int(match._value)
+            addr = value >> 32
+            mask = value & ((1 << 32) - 1)
+            flow_mod.match.set_dl_type(ETHERTYPE_IP)
+            flow_mod.match.set_ipv4_dst_masked(addr, mask)
+        elif match._type == RFMT_IPV6:
+            v = match._value
+            addr = tuple((ord(v[i]) << 8) | ord(v[i + 1])
+                    for i in range(0, 16, 2))
+            mask = tuple((ord(v[i]) << 8) | ord(v[i + 1])
+                    for i in range(16, 32, 2))
+            flow_mod.match.set_dl_type(ETHERTYPE_IPV6)
+            flow_mod.match.set_ipv6_dst_masked(addr, mask)
+        elif match._type == RFMT_ETHERNET:
+            flow_mod.match.set_dl_dst(match._value)
+        elif match._type == RFMT_ETHERTYPE:
+            flow_mod.match.set_dl_type(bin_to_int(match._value))
+        elif match._type == RFMT_NW_PROTO:
+            flow_mod.match.set_ip_proto(bin_to_int(match._value))
+        elif match._type == RFMT_TP_SRC:
+            flow_mod.match.set_ip_proto(IPPROTO_TCP)
+            flow_mod.match.set_tcp_src(bin_to_int(match._value))
+        elif match._type == RFMT_TP_DST:
+            flow_mod.match.set_ip_proto(IPPROTO_TCP)
+            flow_mod.match.set_tcp_dst(bin_to_int(match._value))
+        elif match._type == RFMT_IN_PORT:
+            flow_mod.match.set_in_port(bin_to_int(match._value))
+        elif TLV.optional(match):
+            log.info("Dropping unsupported Match (type: %s)" % match._type)
+        else:
+            log.warning("Failed to serialise Match (type: %s)" % match._type)
+            return
+
 
 def add_actions(flow_mod, action_tlvs):
-  parser = flow_mod.datapath.ofproto_parser
-  ofproto = flow_mod.datapath.ofproto
-  actions = []
-  for a in action_tlvs:
-    action = Action.from_dict(a)
-    if action._type == RFAT_OUTPUT:
-      port = bin_to_int(action._value)
-      a = parser.OFPActionOutput(port, ofproto.OFPCML_MAX)
-      actions.append(a)
-    elif action._type == RFAT_SET_ETH_SRC:
-      srcMac = action._value
-      src = parser.OFPMatchField.make(ofproto.OXM_OF_ETH_SRC, srcMac)
-      actions.append(parser.OFPActionSetField(src))
-    elif action._type == RFAT_SET_ETH_DST:
-      dstMac = action._value
-      dst = parser.OFPMatchField.make(ofproto.OXM_OF_ETH_DST, dstMac)
-      actions.append(parser.OFPActionSetField(dst))
-    elif action.optional():
-        log.info("Dropping unsupported Action (type: %s)" % action._type)
-    else:
-        log.warning("Failed to serialise Action (type: %s)" % action._type)
-        return
-  inst = parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)
-  flow_mod.instructions = [inst]
+    parser = flow_mod.datapath.ofproto_parser
+    ofproto = flow_mod.datapath.ofproto
+    actions = []
+    for a in action_tlvs:
+        action = Action.from_dict(a)
+        if action._type == RFAT_OUTPUT:
+            port = bin_to_int(action._value)
+            a = parser.OFPActionOutput(port, ofproto.OFPCML_MAX)
+            actions.append(a)
+        elif action._type == RFAT_SET_ETH_SRC:
+            srcMac = action._value
+            src = parser.OFPMatchField.make(ofproto.OXM_OF_ETH_SRC, srcMac)
+            actions.append(parser.OFPActionSetField(src))
+        elif action._type == RFAT_SET_ETH_DST:
+            dstMac = action._value
+            dst = parser.OFPMatchField.make(ofproto.OXM_OF_ETH_DST, dstMac)
+            actions.append(parser.OFPActionSetField(dst))
+        elif action.optional():
+            log.info("Dropping unsupported Action (type: %s)" % action._type)
+        else:
+            log.warning("Failed to serialise Action (type: %s)" % action._type)
+            return
+    inst = parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)
+    flow_mod.instructions = [inst]
+
 
 def add_options(flow_mod, options):
-  for o in options:
-    option = Option.from_dict(o)
-    if option._type == RFOT_PRIORITY:
-      flow_mod.priority = bin_to_int(option._value)
-    elif option._type == RFOT_IDLE_TIMEOUT:
-      flow_mod.idle_timeout = bin_to_int(option._value)
-    elif option._type == RFOT_HARD_TIMEOUT:
-      flow_mod.hard_timeout = bin_to_int(option._value)
-    elif option._type == RFOT_CT_ID:
-      pass
-    elif option.optional():
-        log.info("Dropping unsupported Option (type: %s)" % option._type)
-    else:
-        log.warning("Failed to serialise Option (type: %s)" % option._type)
-        return
+    for o in options:
+        option = Option.from_dict(o)
+        if option._type == RFOT_PRIORITY:
+            flow_mod.priority = bin_to_int(option._value)
+        elif option._type == RFOT_IDLE_TIMEOUT:
+            flow_mod.idle_timeout = bin_to_int(option._value)
+        elif option._type == RFOT_HARD_TIMEOUT:
+            flow_mod.hard_timeout = bin_to_int(option._value)
+        elif option._type == RFOT_CT_ID:
+            pass
+        elif option.optional():
+            log.info("Dropping unsupported Option (type: %s)" % option._type)
+        else:
+            log.warning("Failed to serialise Option (type: %s)" % option._type)
+            return
+
 
 def send_pkt_out(dp, port, msg):
-  actions = [dp.ofproto_parser.OFPActionOutput(port, len(msg)), ]
-  dp.send_packet_out(buffer_id=0xffffffff, in_port=dp.ofproto.OFPP_ANY,
-                     actions=actions, data=msg)
+    actions = [dp.ofproto_parser.OFPActionOutput(port, len(msg)), ]
+    dp.send_packet_out(buffer_id=0xffffffff, in_port=dp.ofproto.OFPP_ANY,
+                       actions=actions, data=msg)
--- rfproxy.py
+++ rfproxy.py
@@ -25,60 +25,61 @@ log = logging.getLogger('ryu.app.rfproxy')
 ADD = 1
 DEL = 2
 
+
 #Datapath <-> dp_id association
 class Datapaths:
-  def __init__(self):
-    self.dps = {}   # datapath_id => class Datapath
+    def __init__(self):
+        self.dps = {}     # datapath_id => class Datapath
 
-  def register(self, dp):
-    assert dp.id is not None
-    assert dp.id not in self.dps
-    self.dps[dp.id] = dp
+    def register(self, dp):
+        assert dp.id is not None
+        assert dp.id not in self.dps
+        self.dps[dp.id] = dp
 
-  def unregister(self, dp):
-    if dp.id in self.dps:
-      del self.dps[dp.id]
+    def unregister(self, dp):
+        if dp.id in self.dps:
+            del self.dps[dp.id]
 
-  def get(self, dp_id):
-    return self.dps.get(dp_id, None)
+    def get(self, dp_id):
+        return self.dps.get(dp_id, None)
 
 
 # Association table
 class Table:
-  def __init__(self):
-    self.dp_to_vs = {}
-    self.vs_to_dp = {}
+    def __init__(self):
+        self.dp_to_vs = {}
+        self.vs_to_dp = {}
 
-  def update_dp_port(self, dp_id, dp_port, vs_id, vs_port):
-        # If there was a mapping for this DP port, reset it
-    if (dp_id, dp_port) in self.dp_to_vs:
-      old_vs_port = self.dp_to_vs[(dp_id, dp_port)]
-      del self.vs_to_dp[old_vs_port]
-    self.dp_to_vs[(dp_id, dp_port)] = (vs_id, vs_port)
-    self.vs_to_dp[(vs_id, vs_port)] = (dp_id, dp_port)
+    def update_dp_port(self, dp_id, dp_port, vs_id, vs_port):
+                # If there was a mapping for this DP port, reset it
+        if (dp_id, dp_port) in self.dp_to_vs:
+            old_vs_port = self.dp_to_vs[(dp_id, dp_port)]
+            del self.vs_to_dp[old_vs_port]
+        self.dp_to_vs[(dp_id, dp_port)] = (vs_id, vs_port)
+        self.vs_to_dp[(vs_id, vs_port)] = (dp_id, dp_port)
 
-  def dp_port_to_vs_port(self, dp_id, dp_port):
-    try:
-      return self.dp_to_vs[(dp_id, dp_port)]
-    except KeyError:
-      return None
+    def dp_port_to_vs_port(self, dp_id, dp_port):
+        try:
+            return self.dp_to_vs[(dp_id, dp_port)]
+        except KeyError:
+            return None
 
-  def vs_port_to_dp_port(self, vs_id, vs_port):
-    try:
-      return self.vs_to_dp[(vs_id, vs_port)]
-    except KeyError:
-      return None
+    def vs_port_to_dp_port(self, vs_id, vs_port):
+        try:
+            return self.vs_to_dp[(vs_id, vs_port)]
+        except KeyError:
+            return None
 
-  def delete_dp(self, dp_id):
-    for (id_, port) in self.dp_to_vs.keys():
-      if id_ == dp_id:
-        del self.dp_to_vs[(id_, port)]
+    def delete_dp(self, dp_id):
+        for (id_, port) in self.dp_to_vs.keys():
+            if id_ == dp_id:
+                del self.dp_to_vs[(id_, port)]
 
-    for key in self.vs_to_dp.keys():
-      id_, port = self.vs_to_dp[key]
-      if id_ == dp_id:
-        del self.vs_to_dp[key]
-                    
+        for key in self.vs_to_dp.keys():
+            id_, port = self.vs_to_dp[key]
+            if id_ == dp_id:
+                del self.vs_to_dp[key]
+
     # We're not considering the case of this table becoming invalid when a
     # datapath goes down. When the datapath comes back, the server recreates
     # the association, forcing new map messages to be generated, overriding the
@@ -86,114 +87,119 @@ class Table:
     # If a packet comes and matches the invalid mapping, it can be redirected
     # to the wrong places. We have to fix this.
 
+
 def hub_thread_wrapper(target, args=()):
-    result = hub.spawn(target, *args)
-    result.start = lambda: target
-    return result
+        result = hub.spawn(target, *args)
+        result.start = lambda: target
+        return result
 
 table = Table()
 datapaths = Datapaths()
 
+
 # IPC message Processing
 class RFProcessor(IPC.IPCMessageProcessor):
-  def process(self, from_, to, channel, msg):
-    type_ = msg.get_type()
-    if type_ == ROUTE_MOD:
-      dp = datapaths.get(msg.get_id())
-      ofmsg = create_flow_mod(dp, msg.get_mod(), msg.get_matches(),
-                              msg.get_actions(), msg.get_options())
-      try:
-        dp.send_msg(ofmsg)
-      except Exception as e:
-        log.info("Error sending RouteMod:")
-        log.info(type(e))
-        log.info(str(e))
-      else:
-        log.info("ofp_flow_mod was sent to datapath (dp_id = %s)",
-                 msg.get_id())
-    if type_ == DATA_PLANE_MAP:
-      table.update_dp_port(msg.get_dp_id(), msg.get_dp_port(),
-      msg.get_vs_id(), msg.get_vs_port())
-    return True
+    def process(self, from_, to, channel, msg):
+        type_ = msg.get_type()
+        if type_ == ROUTE_MOD:
+            dp = datapaths.get(msg.get_id())
+            ofmsg = create_flow_mod(dp, msg.get_mod(), msg.get_matches(),
+                                    msg.get_actions(), msg.get_options())
+            try:
+                dp.send_msg(ofmsg)
+            except Exception as e:
+                log.info("Error sending RouteMod:")
+                log.info(type(e))
+                log.info(str(e))
+            else:
+                log.info("ofp_flow_mod was sent to datapath (dp_id = %s)",
+                                 msg.get_id())
+        if type_ == DATA_PLANE_MAP:
+            table.update_dp_port(msg.get_dp_id(), msg.get_dp_port(),
+            msg.get_vs_id(), msg.get_vs_port())
+        return True
 
 
 class RFProxy(app_manager.RyuApp):
-  #Listen to DPSet event which defines datapath enter=true or enter=false
-  _CONTEXTS = {'dpset': dpset.DPSet}
+    #Listen to DPSet event which defines datapath enter=true or enter=false
+    _CONTEXTS = {'dpset': dpset.DPSet}
 
-  OFP_VERSIONS = [ofproto_v1_2.OFP_VERSION]
+    OFP_VERSIONS = [ofproto_v1_2.OFP_VERSION]
 
-  def __init__(self, *args, **kwargs):
-    super(RFProxy, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(RFProxy, self).__init__(*args, **kwargs)
 
-    ID = 0
-    self.ipc = MongoIPC.MongoIPCMessageService(MONGO_ADDRESS, MONGO_DB_NAME, str(ID),
-                                      hub_thread_wrapper, hub.sleep)
-    self.ipc.listen(RFSERVER_RFPROXY_CHANNEL, RFProtocolFactory(), RFProcessor(),
-               False)
-    log.info("RFProxy running.")
+        ID = 0
+        self.ipc = MongoIPC.MongoIPCMessageService(MONGO_ADDRESS,
+                                                   MONGO_DB_NAME, str(ID),
+                                                   hub_thread_wrapper,
+                                                   hub.sleep)
+        self.ipc.listen(RFSERVER_RFPROXY_CHANNEL, RFProtocolFactory(),
+                        RFProcessor(), False)
+        log.info("RFProxy running.")
 
-  #Event handlers
-  @set_ev_cls(dpset.EventDP, dpset.DPSET_EV_DISPATCHER)
-  def handler_datapath(self, ev):
-    dp = ev.dp
-    dpid = dp.id
-    ports = dp.ports
-    if ev.enter:
-      log.info("Datapath is up (dp_id=%s)", dpid_to_str(dpid))
-      datapaths.register(dp)
-      for port in ports:
-        if port <= ofproto_v1_2.OFPP_MAX:
-          msg = DatapathPortRegister(dp_id=dpid, dp_port=port)
-          self.ipc.send(RFSERVER_RFPROXY_CHANNEL, RFSERVER_ID, msg)
-          log.info("Registering datapath port (dp_id=%s, dp_port=%d)", dpid_to_str(dpid), port)
-    elif dpid is not None:
-      log.info("Datapath is down (dp_id=%s)", dpid_to_str(dpid))
-      datapaths.unregister(dp)
-      table.delete_dp(dpid)
-      msg = DatapathDown(dp_id=dpid)
-      self.ipc.send(RFSERVER_RFPROXY_CHANNEL, RFSERVER_ID, msg)
+    #Event handlers
+    @set_ev_cls(dpset.EventDP, dpset.DPSET_EV_DISPATCHER)
+    def handler_datapath(self, ev):
+        dp = ev.dp
+        dpid = dp.id
+        ports = dp.ports
+        if ev.enter:
+            log.info("Datapath is up (dp_id=%s)", dpid_to_str(dpid))
+            datapaths.register(dp)
+            for port in ports:
+                if port <= ofproto_v1_2.OFPP_MAX:
+                    msg = DatapathPortRegister(dp_id=dpid, dp_port=port)
+                    self.ipc.send(RFSERVER_RFPROXY_CHANNEL, RFSERVER_ID, msg)
+                    log.info("Registering datapath port (dp_id=%s,"
+                             "dp_port=%d)", dpid_to_str(dpid), port)
+        elif dpid is not None:
+            log.info("Datapath is down (dp_id=%s)", dpid_to_str(dpid))
+            datapaths.unregister(dp)
+            table.delete_dp(dpid)
+            msg = DatapathDown(dp_id=dpid)
+            self.ipc.send(RFSERVER_RFPROXY_CHANNEL, RFSERVER_ID, msg)
 
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def on_packet_in(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+        dpid = dp.id
+        dst, src, _eth_type = struct.unpack_from('!6s6sH', buffer(msg.data), 0)
+        for f in msg.match.fields:
+            if f.header == ofproto_v1_2.OXM_OF_IN_PORT:
+                in_port = f.value
 
-  @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-  def on_packet_in(self, ev):
-    msg = ev.msg
-    dp = msg.datapath
-    dpid = dp.id
-    dst, src, _eth_type = struct.unpack_from('!6s6sH', buffer(msg.data), 0)
-    for f in msg.match.fields:
-      if f.header == ofproto_v1_2.OXM_OF_IN_PORT:
-        in_port = f.value
+        # If we have a mapping packet, inform RFServer through a Map message
+        if _eth_type == RF_ETH_PROTO:
+            vm_id, vm_port = struct.unpack("QB", msg.data[14:23])
+            log.info("Received mapping packet (vm_id=%s, vm_port=%d, "
+                     "vs_id=%s, vs_port=%d)", format_id(vm_id), vm_port,
+                     dpid_to_str(dpid), in_port)
+            msg = VirtualPlaneMap(vm_id=vm_id, vm_port=vm_port, vs_id=dpid,
+                                  vs_port=in_port)
+            self.ipc.send(RFSERVER_RFPROXY_CHANNEL, RFSERVER_ID, msg)
+            return
 
-    # Drop all LLDP packets
-    #ADD LLDP_TYPE to ryu.ofproto.ether
-    #if _eth_type == LLDP_TYPE:
-    #    return
-        
-    # If we have a mapping packet, inform RFServer through a Map message
-    if _eth_type == RF_ETH_PROTO:
-      vm_id, vm_port = struct.unpack("QB", msg.data[14:23])
-      log.info("Received mapping packet (vm_id=%s, vm_port=%d, vs_id=%s, vs_port=%d)",
-            format_id(vm_id), vm_port, dpid_to_str(dpid), in_port)
-      msg = VirtualPlaneMap(vm_id=vm_id, vm_port=vm_port, vs_id=dpid, vs_port=in_port)
-      self.ipc.send(RFSERVER_RFPROXY_CHANNEL, RFSERVER_ID, msg)
-      return
-
-    # If the packet came from RFVS, redirect it to the right switch port
-    if is_rfvs(dpid):
-      dp_port = table.vs_port_to_dp_port(dpid, in_port)
-      if dp_port is not None:
-        dp_id, dp_port = dp_port
-        send_pkt_out(datapaths.get(dp_id), dp_port, msg.data)
-        log.info("forwarding packet from rfvs (dp_id: %s, dp_port: %d)", dpid_to_str(dp_id), dp_port)
-      else:
-        log.info("Unmapped RFVS port (vs_id=%s, vs_port=%d)", dpid_to_str(dpid), in_port)
-    # If the packet came from a switch, redirect it to the right RFVS port
-    else:
-      vs_port = table.dp_port_to_vs_port(dpid, in_port)
-      if vs_port is not None:
-        vs_id, vs_port = vs_port
-        send_pkt_out(datapaths.get(vs_id), vs_port, msg.data)
-        log.info("forwarding packet to rfvs (vs_id: %s, vs_port: %d)", dpid_to_str(vs_id), vs_port)
-      else:
-        log.info("Unmapped datapath port (dp_id=%s, dp_port=%d)", dpid_to_str(dpid), in_port)
+        # If the packet came from RFVS, redirect it to the right switch port
+        if is_rfvs(dpid):
+            dp_port = table.vs_port_to_dp_port(dpid, in_port)
+            if dp_port is not None:
+                dp_id, dp_port = dp_port
+                send_pkt_out(datapaths.get(dp_id), dp_port, msg.data)
+                log.info("forwarding packet from rfvs (dp_id: %s, "
+                         "dp_port: %d)", dpid_to_str(dp_id), dp_port)
+            else:
+                log.info("Unmapped RFVS port (vs_id=%s, vs_port=%d)",
+                         dpid_to_str(dpid), in_port)
+        # If the packet came from a switch, redirect it to the right RFVS port
+        else:
+            vs_port = table.dp_port_to_vs_port(dpid, in_port)
+            if vs_port is not None:
+                vs_id, vs_port = vs_port
+                send_pkt_out(datapaths.get(vs_id), vs_port, msg.data)
+                log.info("forwarding packet to rfvs (vs_id: %s, vs_port: %d)",
+                         dpid_to_str(vs_id), vs_port)
+            else:
+                log.info("Unmapped datapath port (dp_id=%s, dp_port=%d)",
+                         dpid_to_str(dpid), in_port)
